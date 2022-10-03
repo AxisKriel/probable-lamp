f4cf51939c7312d5fd53c0f05a9f6dea3ce1d969
Rodrigo <rodrigo.rente98@gmail.com>
2022-10-03 16:49:46 +0100

    Test and method for registering app params

--- Source/src/PortableLamp/App.java
+++ Source/src/PortableLamp/App.java
@@ -1,7 +1,56 @@
 package PortableLamp;
 
+import java.util.Scanner;
+
 public class App {
+
+    public int n;
+    public String text;
+
     public static void main(String[] args) throws Exception {
         
+    }
+
+    /**
+     * Populates the parameters of the app (n and text) based on the input.
+     * @param args Used if not empty, otherwise, the input stream is used.
+     */
+    public void getArgs(String[] args) {
+        // if no args are passed beforehand, we get them from user input
+        if (args.length == 0) {
+            var input = "";
+            try (var scanner = new Scanner(System.in)) {
+                while (scanner.hasNext()) {
+                    // load the full input
+                    input += scanner.nextLine();
+                }
+            }
+            args = input.split(" ");
+        }
+
+        String syntaxError = "Invalid syntax. Correct syntax: [n:number] <text...>";
+        if (args.length < 2) {
+            System.out.println(syntaxError);
+            return;
+        }
+
+        // get N from the first parameter
+        try {
+            this.n = Integer.parseInt(args[0]);
+        }
+        catch (NumberFormatException exception) {
+            System.out.println(syntaxError);
+            return;
+        }
+
+        // get text from the remainder
+        String textArgs = "";
+        for (int i = 1; i < args.length; i++) {
+            textArgs += args[i];
+            if (i < args.length-1) {
+                textArgs += " ";
+            }
+        }
+        this.text = textArgs;
     }
 }
--- Source/src/Tests.java
+++ Source/src/Tests.java
@@ -1,5 +1,26 @@
+import java.io.ByteArrayInputStream;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
 import PortableLamp.App;
 
 public class Tests {
-    
+
+    @Test
+    public void testGetArgs() {
+        var app = new App();
+
+        // a couple test inputs to ensure the app works as intended
+        String input1 = "3 ITClinical";                 // n = 3 
+        String input2 = "ITClinical";                   // n = 1 (default)
+        String input3 = "2 This is a Longer Phrase";    // n = 2
+        String input4 = "Multiple Words Without N";     // n = 1
+
+        // first test - n should be 3 and text should be ITClinical
+        System.setIn(new ByteArrayInputStream(input1.getBytes()));
+        app.getArgs(new String[] {});
+        assertEquals(app.n, 3);
+        assertEquals(app.text, "ITClinical");
+    }
 }
